5fd739fbeea630049221e95eaba0785f0c872e77
Ethan Harlig <ethan.harlig@gmail.com>
2022-11-06 00:23:15 -0700

    We call that a working menu!!

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    // ensure this is properly set otherwise the play button won't work!
+    public int gameLevelBuildIndex;
+
+
+    public void PlayGame() {
+        Debug.LogFormat("If play button is not working, is this the correct game level build index: {0}!", this.gameLevelBuildIndex);
+        SceneManager.LoadScene(this.gameLevelBuildIndex);
+
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+
+        DeselectCurrentlySelectedGameObject();
+
+    }
+
+    public void DeselectCurrentlySelectedGameObject() {
+        GameObject.Find("EventSystem").GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(null);
+    }
+}
