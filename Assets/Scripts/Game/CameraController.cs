8b646b5d18ff68330efc3e761c92388e429cdb99
Ethan Harlig <ethan.harlig@gmail.com>
2022-11-06 16:45:52 -0800

    Coroutine for CameraController

--- Assets/Scripts/Game/CameraController.cs
+++ Assets/Scripts/Game/CameraController.cs
@@ -20,15 +20,8 @@ public class CameraController : MonoBehaviour
     private bool _isRotating = false;
     // OnRotate comes from the InputActions action defined Rotate
     void OnRotate(InputValue movementValue) {
-        if (_isRotating) {
-            Debug.Log("Tried to call rotate when _isRotating is true");
-            return;
-        }
-
-        _isRotating = true;
         float moveDirection = movementValue.Get<float>();
 
-        // TODO  doesn't work once player has moved. Should just center on map?
         if (moveDirection > 0) {
             Debug.Log("positive move direction");
             Vector3 rot = this.transform.eulerAngles;
@@ -38,26 +31,31 @@ public class CameraController : MonoBehaviour
             Vector3 rot = this.transform.eulerAngles;
             this.targetEulerAngles = new Vector3(rot.x, rot.y + 90, rot.z);
         }
-    }
-
-    public void FixedUpdate() {
         if (_isRotating) {
-            Rotate();
+            Debug.Log("Tried to call rotate when _isRotating is true");
+            return;
         }
+
+        _isRotating = true;
+        StartCoroutine(Rotate());
     }
 
-    private void Rotate() {
-        if (progress == 0) {
-            startRotation = this.transform.rotation;
-        }
-        this.transform.rotation = Quaternion.Lerp(startRotation, Quaternion.Euler(targetEulerAngles), progress);
+    private IEnumerator Rotate() {
+        startRotation = this.transform.rotation;
+        while (_isRotating && progress < 1) {
+            this.transform.rotation = Quaternion.Lerp(startRotation, Quaternion.Euler(targetEulerAngles), progress);
 
-        // move with frame rate
-        progress += spd * Time.fixedDeltaTime;
-        if (progress >= 1) {
-            _isRotating = false;
-            progress = 0;
+            // move with frame rate
+            progress += spd * Time.fixedDeltaTime;
+
+            // halt iteration until next frame
+            yield return null;
         }
+
+        _isRotating = false;
+        progress = 0;
+
+        yield return null;
     }
 
 }
