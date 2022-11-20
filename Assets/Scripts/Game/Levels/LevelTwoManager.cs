c6ab7bb8d04bdf8af02567d03fa5369cae7f68c5
Ethan Harlig <ethan.harlig@gmail.com>
2022-11-19 18:41:34 -0800

    Level2 easier

--- Assets/Scripts/Game/Levels/LevelTwoManager.cs
+++ Assets/Scripts/Game/Levels/LevelTwoManager.cs
@@ -11,7 +11,7 @@ public class LevelTwoManager : LevelManager
     void Start()
     {
         gridSizeX = gridSizeY = 10;
-        turnLimit = 99;
+        turnLimit = 5;
 
         SetupLevel();
 
