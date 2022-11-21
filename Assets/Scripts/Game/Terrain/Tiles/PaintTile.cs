2e30c7052df06c611eff58c615e5913c2c102806
Ethan Harlig <ethan.harlig@gmail.com>
2022-11-20 18:30:53 -0800

    PaintTile refactor

--- Assets/Scripts/Game/Terrain/Tiles/PaintTile.cs
+++ Assets/Scripts/Game/Terrain/Tiles/PaintTile.cs
@@ -1,17 +1,10 @@
 using UnityEngine;
 
-// TODO refactor into PaintTile class with like TileController interface
-// can also have IceTile, TrapTile, etc.
-public class TileController : MonoBehaviour
+public class PaintTile : TileController
 {
     public bool IsPainted
     {
         get; private set;
-    }
-
-    public GameObject GetTile()
-    {
-        return gameObject;
     }
 
     public Color GetColor()
