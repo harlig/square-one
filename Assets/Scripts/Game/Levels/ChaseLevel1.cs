072c6cb4f16bf8bec05b1249a3a3184d7272fb42
Sam Rastovich <sam@thinglogix.com>
2022-11-26 19:50:06 -0800

    Refactor ChaseLevel1 with GSM

--- Assets/Scripts/Game/Levels/ChaseLevel1.cs
+++ Assets/Scripts/Game/Levels/ChaseLevel1.cs
@@ -4,23 +4,8 @@ using UnityEngine;
 // simple chase level that introduces player to the concept 
 public class ChaseLevel1 : LevelManager
 {
-    private List<GameState> gameStateOrder;
-    private GameState currentGameState;
 
     private List<MovingObstacle> obstacles;
-
-    enum GameState
-    {
-        START,
-        GREEN_SETUP,
-        GREEN_HIT,
-        RED_SETUP,
-        RED_HIT,
-        BLUE_SETUP,
-        BLUE_HIT,
-        SUCCESS,
-        FAILED,
-    };
 
 #pragma warning disable IDE0051
     void Start()
@@ -28,19 +13,19 @@ public class ChaseLevel1 : LevelManager
         gridSizeX = gridSizeY = 11;
         turnLimit = 70;
 
-        gameStateOrder = new List<GameState>
-        {
-            GameState.START,
-            GameState.GREEN_SETUP,
-            GameState.GREEN_HIT,
-            GameState.RED_SETUP,
-            GameState.RED_HIT,
-            GameState.BLUE_SETUP,
-            GameState.BLUE_HIT,
-            GameState.SUCCESS
+        SetupLevel(gridSizeX / 2 + 4, gridSizeY / 2);
+
+        Vector2Int[] waypointsInOrder = new[] {
+            new Vector2Int(9, 9),
+            new Vector2Int(0, 0),
+            new Vector2Int(4, gridSizeY - 1),
+            new Vector2Int(squareOne.x, squareOne.y)
         };
 
-        SetupLevel(gridSizeX / 2 + 4, gridSizeY / 2);
+        gsm.SetWaypoints(waypointsInOrder, true);
+        gsm.SetTurnLimit(turnLimit);
+
+        gsm.ManageGameState();
 
         obstacles = new List<MovingObstacle>();
 
@@ -51,41 +36,14 @@ public class ChaseLevel1 : LevelManager
         gridController.PaintTileAtLocation(new Vector2Int(1, gridSizeY - 2), Color.white);
 
         obstacles.Add(obstacle);
-
-        currentGameState = GameState.START;
     }
 
-    void Update()
-    {
-        SetMoveCountText();
-        if (levelActive)
-        {
-            ManageGameState();
-        }
-    }
 #pragma warning restore IDE0051
 
-    override protected void OnPlayerMoveFinish(Vector2Int playerPosition)
+    override protected void OnPlayerMoveFinishWithShouldCountMove(Vector2Int playerPosition, bool shouldCountMove)
     {
-        if (levelActive)
-        {
-            turnsLeft = turnLimit - playerController.GetMoveCount();
-        }
-    }
-
-    void ManageGameState()
-    {
-        Vector2Int playerPos = playerController.GetCurrentPosition();
-
-        // allow devMode to not fall out of map
-        if (!gridController.IsWithinGrid(playerPos))
-        {
-            Debug.Log("Player has exited map.");
-            currentGameState = GameState.FAILED;
-        }
-
         // white tile color disables all moving obstacles
-        if (gridController.TileColorAtLocation(playerPos) == Color.white)
+        if (gridController.TileColorAtLocation(playerPosition) == Color.white)
         {
             Debug.Log("Stopping obstacle movement!");
             foreach (MovingObstacle obstacle in obstacles)
@@ -94,73 +52,9 @@ public class ChaseLevel1 : LevelManager
             }
         }
 
-        // game state handler
-        switch (currentGameState)
+        if (shouldCountMove)
         {
-            case GameState.START:
-                // secret location? kinda shitty to make player guess and check
-                if (playerPos.x == 9 && playerPos.y == 9)
-                {
-                    TransitionState();
-                }
-                break;
-            case GameState.GREEN_SETUP:
-                gridController.PaintTileAtLocation(0, 0, Color.green);
-                TransitionState();
-                break;
-            case GameState.GREEN_HIT:
-                if (gridController.TileColorAtLocation(playerPos) == Color.green)
-                {
-                    TransitionState();
-                }
-                break;
-            case GameState.RED_SETUP:
-                gridController.PaintTileAtLocation(4, gridSizeY - 1, Color.red);
-                TransitionState();
-                break;
-            case GameState.RED_HIT:
-                if (gridController.TileColorAtLocation(playerPos) == Color.red)
-                {
-                    TransitionState();
-                }
-                break;
-            case GameState.BLUE_SETUP:
-                // last step is back to square one
-                gridController.PaintTileAtLocation(squareOne.x, squareOne.y, Color.blue);
-                TransitionState();
-                break;
-            case GameState.BLUE_HIT:
-                if (gridController.TileColorAtLocation(playerPos) == Color.blue)
-                {
-                    TransitionState();
-                    // you must manage game state here before falling through, otherwise you could be transitioning
-                    // into a success game state when you're at zero turns!
-                    ManageGameState();
-                }
-                break;
-            case GameState.SUCCESS:
-                Debug.Log("Player has won!");
-                SetTerminalGameState(successElements);
-                break;
-            case GameState.FAILED:
-                Debug.Log("Player has failed.");
-                SetTerminalGameState(failedElements);
-                break;
-            default:
-                Debug.LogErrorFormat("Encountered unexpected game state: {0}", currentGameState);
-                break;
-        }
-
-        if (currentGameState != GameState.SUCCESS && turnsLeft <= 0)
-        {
-            Debug.Log("Player exceeded move count");
-            currentGameState = GameState.FAILED;
-        }
-
-        void TransitionState()
-        {
-            // could probably use a better data structure as the state machine that allows a failure state as defined by the state machine
-            currentGameState = gameStateOrder[gameStateOrder.IndexOf(currentGameState) + 1];
+            turnsLeft = turnLimit - playerController.GetMoveCount();
         }
     }
 }
