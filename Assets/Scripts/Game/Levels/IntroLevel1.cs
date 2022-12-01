3c569afca10fe0ed20c8325f117311f65ef6ab37
Ethan Harlig <ethan.harlig@gmail.com>
2022-11-30 23:13:45 -0800

    Making intro level 1 more interesting

--- Assets/Scripts/Game/Levels/IntroLevel1.cs
+++ Assets/Scripts/Game/Levels/IntroLevel1.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -9,7 +11,7 @@ public class IntroLevel1 : LevelManager
 
     void Start()
     {
-        gridSizeX = gridSizeY = 7;
+        gridSizeX = gridSizeY = 8;
         // turnLimit = 30;
 
         SetupLevel();
@@ -21,18 +23,27 @@ public class IntroLevel1 : LevelManager
             .WithOnTriggeredAction(() => {
                 helpText.text = "Move with WASD or arrow keys";
                 helpElement.SetActive(true);
+                StartCoroutine(WaitForNumberOfMoves(3, () => {
+                    helpText.text = "Your obective is to collect all of the waypoints";
+                    gsm.SpawnNextWaypoint();
+                }));
+            }),
+            Waypoint.Of(1, 1)
+            .WithOnTriggeredAction(() => {
+                helpElement.SetActive(true);
                 gsm.SpawnNextWaypoint();
             }),
             Waypoint.Of(gridSizeX - 2, 0)
             .WithOnTriggeredAction(() => {
                 helpElement.SetActive(false);
+                helpText.text = "Rotate the camera with Q/E\nThe compass in the bottom left shows your orientation";
                 gsm.SpawnNextWaypoint();
             }),
             Waypoint.Of(2, gridSizeY - 1),
             Waypoint.Of(1, 2)
             .WithOnTriggeredAction(() => {
-                helpText.text = "Rotate the camera with Q/E\nSome waypoints can be hard to find";
                 helpElement.SetActive(true);
+                helpText.text = "Some waypoints can be hard to find!";
                 gsm.SpawnNextWaypoint();
             }),
             Waypoint.Of(gridSizeX - 1, gridSizeY - 2).WithOptions(Waypoint.WaypointOptions.Of(0.15f))
@@ -52,5 +63,12 @@ public class IntroLevel1 : LevelManager
         gridController.AddStationaryObstacleAtPosition(gridSizeX - 2, gridSizeY - 2);
         gridController.AddStationaryObstacleAtPosition(gridSizeX - 3, gridSizeY - 2);
         gridController.AddStationaryObstacleAtPosition(gridSizeX - 2, gridSizeY - 3);
+    }
+
+    IEnumerator WaitForNumberOfMoves(int movesToWait, Action afterWaitAction)
+    {
+        int desiredMoveCount = playerController.GetMoveCount() + movesToWait;
+        yield return new WaitWhile(() => playerController.GetMoveCount() < desiredMoveCount);
+        afterWaitAction?.Invoke();
     }
 }
